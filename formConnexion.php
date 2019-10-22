cd9bdb73ff30c26dd1559b49a6cfec008a766503
Alban <albandany1@gmail.com>
2019-10-22 17:29:34 +0200

    Connexion fonctionelle

--- formConnexion.php
+++ formConnexion.php
@@ -1,14 +1,26 @@
 <?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=siteanimaux;charset=utf8', 'root', '');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
+
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'siteanimaux');
+if ($mysqli->connect_errno) {
+    echo "Désolé, le site web subit des problèmes.";
+    echo "Error: Échec d'établir une connexion MySQL, voici pourquoi : \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    exit;
 }
 
 $user = $_POST['user'];
 $mdp = $_POST['mdp'];
-$test; // Vrai si l'user et le mdp match
+
+$sql = 'SELECT mdp  FROM utilisateur WHERE nom="'.$_POST['user'].'"';
+$result = $mysqli->query($sql) or die($mysqli->error);
+$mdpbdd = mysqli_fetch_array($result,MYSQLI_NUM);
+	
+if (isset($_POST['mdp']) AND $_POST['mdp'] == $mdpbdd[0]){
+	echo "hello there";
+}
+else{
+	echo "Mauvais mot de passe";	
+}
+
 ?>
