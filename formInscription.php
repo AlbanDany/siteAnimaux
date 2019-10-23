e718e99830074d1e224029089dc421ec2e52c108
Alban <albandany1@gmail.com>
2019-10-23 16:26:09 +0200

    Inscription Fonctionelle

--- formInscription.php
+++ formInscription.php
@@ -2,9 +2,9 @@
 	require_once 'connexionbdd.php';
 	
 	$user = $_POST['utilisateur'];
-	$mdp = password_hash('"'.$_POST['motdepasse'].'"', PASSWORD_BCRYPT);
+	$mdp = password_hash($_POST['motdepasse'], PASSWORD_BCRYPT);
 
-
+	$sql = 'INSERT INTO utilisateur (idUser, nom, mdp, idBestiaire) VALUES (NULL, "'.$user.'", "'.$mdp.'", NULL)';
 
 	if (empty($_POST['utilisateur']) || empty($_POST['motdepasse']) ) //Oublie d'un champ
 	{
@@ -12,9 +12,9 @@
 	}
 	else {
 		
-		$sql = 'INSERT INTO utilisateur (idUser, nom, mdp, idBestiaire) VALUES (NULL, "'.$user.'", "'.$mdp.'", NULL)';
+		
 		$result = $mysqli->query($sql) or die($mysqli->error);
-		$message = "Utilisateur inscrit"
+		$message = "Utilisateur inscrit";
 	}		
 
 	header("Location: inscription.php?message=$message"); //Revoie direct a la page de connexion
